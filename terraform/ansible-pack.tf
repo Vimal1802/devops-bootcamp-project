3454dad9018fe77974951e2a5a7a7e765973248f
Vimal1802 <98274827+Vimal1802@users.noreply.github.com>
2026-01-14 19:59:33 +0800

    Update ansible-pack.tf

--- terraform/ansible-pack.tf
+++ terraform/ansible-pack.tf
@@ -1,6 +1,4 @@
-# 1. We NO LONGER need tls_private_key or local_file for the .pem
-
-# 2. Pack the Ansible folder
+# 1. Pack the Ansible folder
 # This will now only contain your playbooks, roles, and config files
 data "archive_file" "ansible_pack" {
   type        = "zip"
@@ -10,7 +8,7 @@ data "archive_file" "ansible_pack" {
   # Removed: depends_on = [local_file.private_key_pem]
 }
 
-# 3. Encode the zip for the User Data
+# 2. Encode the zip for the User Data
 locals {
   ansible_zip_data = (data.archive_file.ansible_pack.output_base64sha256 != "") ? filebase64(data.archive_file.ansible_pack.output_path) : ""
 }
